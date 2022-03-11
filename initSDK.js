472e1fd670d040d438b5a62ce33bb47ee6ae5751
Kiera <kwalton@liveperson.com>
2022-03-11 14:08:44 +0000

    changes

--- initSDK.js
+++ initSDK.js
@@ -1,5 +1,4 @@
-const customerMessages = [];
-
+const consumerMessage = [];
 
 // code onlyruns once DOM is ready for JS code to operate
   $(document).ready(function () {
@@ -8,12 +7,9 @@ const customerMessages = [];
     var updateCallback = function(data) {
       var value = data.newValue;
       value.forEach( item => {
-        // if(item.source == 'visitor') {
-        //   customerMessages.push(item.text);
-        // }
-        customerMessages.push(item.text);
+        consumerMessage.push(item.text);
       });
-      console.log("customer messages: ", customerMessages);
+      console.log("messages from consumer: ", consumerMessage);
     };
 
     // chatTranscript is an SDK sub category
@@ -22,46 +18,27 @@ const customerMessages = [];
     // binding: first receive callback w existing data & then receive updates of current data
       lpTag.agentSDK.bind(pathToData, updateCallback);
 
-      // $ is short for get element by ID
       $('#consumer-last-message').click(function () {
-        var c = "t="+customerMessages[customerMessages.length - 1];
+        var c = "t="+consumerMessage[consumerMessage.length - 1];
         console.log(c);
         var d = 'http://www.omdbapi.com/?' + c;
         var e = $('#request');
         e.find('a').attr('href', d).html(d);
         var f = $('#progress');
-        f.show('slow');
         var g = $('#response');
         var t = $('input:hidden[name=g-recaptcha-response]').val();
         $.ajax({
             type: 'GET',
             dataType: 'text',
-            url: 'https://www.omdbapi.com/?apikey=1a59b8e9&' + c + '&token=' + t,
-            statusCode: {
-                403: function () {
-                    g.find('pre').html('HTTP 403 Forbidden!')
-                    notify.setAlertMessageRed("Error getting data");
-                }
-            },
+            url: 'https://www.omdbapi.com/?apikey=37db681f&' + c + '&token=' + t,
+           
             success: function (a) {
                 g.find('table').empty();
                 var data = JSON.parse(a);
                 const keys = Object.keys(data);
                 keys.forEach(key => {
-                  if (key == 'Ratings') {
-                    g.find('table').append(`<tr><td><strong>${key}</strong></td></tr>`);
-                    const ratings = data[key];
-                    ratings.forEach(rating => {
-                      g.find('table').append(`<tr><td><strong>- ${rating.Source}</strong></td></tr><tr><td>${rating.Value}</td></tr>`);
-                    });
-                  } else {
-                    g.find('table').append(`<tr><td><strong>${key}</strong></td></tr><tr><td>${data[key]}</td></tr>`);
-                  }
+                  g.find('table').append(`<tr><td>${key}</td></tr><tr><td>${data[key]}</td></tr>`);
                 });
-            },
-            complete: function () {
-                f.hide();
-                g.show('slow')
             }
         })
       });
--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 $(document).ready(function () {
 
+  // reset button
+  $('#movie-reset').click(function () {
+    var a = $('#request');
+    a.empty()
+    var b = $('#progress');
+    b.empty()
+    var c = $('#response');
+    c.find('table').empty()
+  });
+
   // search by film title
     var a = $('#request');
-    a.hide(0);
     a.find('a').attr('href', 'javascript:;').html('');
     var b = $('#progress');
-    b.hide(0);
     var c = $('#response');
-    c.hide(0);
-    c.find('pre').html('');
   
     // search by film title button
     $('#movie-title-button').click(function () {
@@ -20,53 +26,25 @@ $(document).ready(function () {
       var e = $('#request');
       e.find('a').attr('href', d).html(d);
       var f = $('#progress');
-      f.show('slow');
       var g = $('#response');
       var t = $('input:hidden[name=g-recaptcha-response]').val();
       $.ajax({
           type: 'GET',
           dataType: 'text',
           // URL for API request using my generated API key & the movie name 
-          //url: 'http://www.omdbapi.com/?apikey=37db681f&' + c + '&token=' + t,
-          url: 'https://www.omdbapi.com/?apikey=1a59b8e9&' + c + '&token=' + t,
-          statusCode: {
-              403: function () {
-                  g.find('pre').html('HTTP 403 Forbidden!')
-              }
-          },
+          url: 'https://www.omdbapi.com/?apikey=37db681f&' + c + '&token=' + t,
+          
+          // populating the table with the data on success
           success: function (a) {
               g.find('table').empty();
               var data = JSON.parse(a);
               const keys = Object.keys(data);
               keys.forEach(key => {
-                if (key == 'Ratings') {
-                  g.find('table').append(`<tr><td><strong>${key}</strong></td></tr>`);
-                  const ratings = data[key];
-                  ratings.forEach(rating => {
-                    g.find('table').append(`<tr><td><strong>- ${rating.Source}</strong></td></tr><tr><td>${rating.Value}</td></tr>`);
-                  });
-                } else {
-                  g.find('table').append(`<tr><td><strong>${key}</strong></td></tr><tr><td>${data[key]}</td></tr>`);
-                }
+                g.find('table').append(`<tr><td>${key}</td></tr><tr><td>${data[key]}</td></tr>`);
               });
-          },
-          complete: function () {
-              f.hide();
-              g.show('slow')
           }
       })
     });
 
-  // reset button
-    $('#movie-reset').click(function () {
-        var a = $('#request');
-        a.hide('slow');
-        a.find('a').attr('href', 'javascript:;').html('');
-        var b = $('#progress');
-        b.hide('slow');
-        var c = $('#response');
-        c.find('table').empty()
-        c.hide('slow');
-        c.find('pre').html('');
-    });
+  
   });
